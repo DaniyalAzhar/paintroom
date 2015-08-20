709d0e26097b31a9101cc80288eec4944d15b28e
DaniyalAzhar <11bscsdmalik@seecs.edu.pk>
2015-08-21 00:09:20 +0500

    adding files

---
+++ resources/views/default.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laravel Auth Tutorial</title>
+</head>
+<body>
+<div class="container">
+    ��<div>
+        ����<h1>Welcome to paintroom</h1>
+        ��</div>
+    @yield('content')
+</div>
+</body>
+</html>
